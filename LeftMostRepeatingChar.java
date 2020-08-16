5a084c4e03e245078617a9b55f8fdc48d90f829c
Reach-VJ <66485684+Reach-VJ@users.noreply.github.com>
2020-08-15 20:25:07 -0700

    Add files via upload

--- LeftMostRepeatingChar.java
+++ LeftMostRepeatingChar.java
@@ -3,8 +3,8 @@ package com.reachingskies.geeksandgeeks.strings;
 public class LeftMostRepeatingChar {
 
     public static void main(String [] args) {
-        String str = "hello";
-        int index = LeftMostRepeatingChar.leftMostRepeatingChar(str);
+        String str = "heollo";
+        int index = LeftMostRepeatingChar.leftMostRepeatingCharOptimal(str);
 
         System.out.println("Left most repeated character --> "+ str.charAt(index));
     }
@@ -30,5 +30,27 @@ public class LeftMostRepeatingChar {
             }
         }
         return -1;
+    }
+
+    // Input : "heollo" o is the first character appears more than 1 time.
+    // Output : 2 (or o) index of o in str.
+    // Time : O(n)
+    // Space : O(n) --> to save frequencies of chars.
+    static int leftMostRepeatingCharOptimal(String str) {
+
+        if(str == null || str.length() == 0)
+            return -1;
+        int index = -1;
+        boolean [] visited = new boolean[256];
+        //Assign i to index if char appears second time.
+        // Why iterating back to front --> to find left most repeated char.
+        // It will print l if iterate front to back.
+        for(int i = str.length()-1; i >= 0; i--) {
+            if(visited[str.charAt(i)] == true)
+                index = i;
+            else
+                visited[str.charAt(i)] = true;
+        }
+        return index;
     }
 }
